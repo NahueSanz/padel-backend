850665c5d2184b2ca046d576e45d0316b2e08fd4
NahueSanz <nahue.p.sanz@gmail.com>
2025-09-16 23:30:11 -0300

    Día 1: proyecto backend inicial con Spring Boot

---
+++ src/test/java/com/padel/social/padel_backend/PadelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padel.social.padel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
